c7aa8197f82e0c7f138d113bf79c939f7f60ca91
awemulya <awemulya@gmail.com>
2016-08-26 10:12:55 +0545

    power args are int only

--- source/recursion/power.py
+++ source/recursion/power.py
@@ -2,6 +2,10 @@ class Power(object):
     def __init__(self, x, n):
         """ calculates power of x to the power x ^n
         """
+        if not isinstance(x,int):
+            raise TypeError("x must be type Int")
+        if not isinstance(n,int):
+            raise TypeError("n must be type int")
         self._x = x
         self._n = n
         self._result = self.calculate_power()
